dc5caca5deecd380d4864683cf607769ca70a988
gennadiySinigur <gennadiysinigur@gmail.com>
2022-12-25 17:48:07 +0200

    Created Post class

---
+++ src/main/java/com/gennadiysinigur/blog/models/Post.java
@@ -0,0 +1,71 @@
+package com.gennadiysinigur.blog.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String announce;
+
+    private String fullText;
+
+    private int views;
+
+    public Post() {
+    }
+
+    public Post(String title, String announce, String fullText) {
+        this.title = title;
+        this.announce = announce;
+        this.fullText = fullText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnnounce() {
+        return announce;
+    }
+
+    public void setAnnounce(String announce) {
+        this.announce = announce;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+}
